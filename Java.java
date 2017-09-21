9a9a6c9d2b268ce1e864c6a0fa21757a8057149b
Computer Society IEEE - UFABC <ieeecsufabc@gmail.com>
2017-09-21 20:02:39 -0300

    Exercise 1

---
+++ Java.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Java {
+	public static void main(String[] args) {
+		String cor;
+		int dias;
+		float resultado;
+		if(String == Verde){
+			resultado = dias * 2.50;
+		}
+		if(String == Amarelo){
+			resultado = dias * 2.70;
+		}
+		if(String == Branco){
+			resultado = dias * 2.00;
+		}
+		if(String == Rosa){
+			resultado = dias * 3.00;
+		}
+		printf(O resultado é "resultado")
+	}
+}
